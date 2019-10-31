cafbb5007463e4e6f630c8604d6c6c2cf1b8f0c1
chad svastisalee <swasgrafx@gmail.com>
2019-10-30 19:35:50 -0500

    Chat App Socket.io Styled

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+// Use to see User Connect or Disconnect
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+// });
+
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
       io.emit('chat message', msg);
